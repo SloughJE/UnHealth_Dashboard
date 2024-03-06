3af8d492921e17f1bc0980e5c6c69fd74d1a789c
SloughJE <SloughJE@gmail.com>
2024-03-06 13:50:09 -0500

    update format

--- src/tabs/ai_patient_view_tab.py
+++ src/tabs/ai_patient_view_tab.py
@@ -15,7 +15,7 @@ def ai_patient_view_tab_layout():
 
     layout = dbc.Container([
         html.P("Explore simulated patient data using an AI-generated patient summary integrated with the UnHealth Score, with various charts of vital signs, lab results, and quality of life metrics.", 
-            className='tab-description', style={'textAlign': 'center', 'marginBottom': '20px'}),
+            className='tab-description', style={'textAlign': 'center', 'marginBottom': '10px','marginTop': '30px'}),
         html.Div([
             html.Button('Generate Data from Random Patient', id='generate-random-patient-button', className='custom-button-ai'),
         ], style={'display': 'flex', 'justifyContent': 'center', 'marginBottom': '20px'}),
